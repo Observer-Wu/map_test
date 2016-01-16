3a1bf11ad618f5bedbe6608c848b6fde92a3794f
ryan <ryan_wu@ryandeMacBook-Pro.local>
2016-01-16 22:56:04 +0800

    run the basic map

---
+++ app/src/main/java/com/example/ryan_wu/map_test/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.ryan_wu.map_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
+
+public class MainActivity extends AppCompatActivity {
+    private MapView mapView;
+    private AMap aMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mapView = (MapView) findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+        aMap = mapView.getMap();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+}
